2486d7db66591c3ebbb79137ef7bdeab88193dba
Bassmbacke <dioufstory@gmail.com>
2025-07-18 18:15:12 +0200

    all

--- all_events.js
+++ all_events.js
@@ -1,19 +1,6 @@
 if (typeof events === 'undefined') var events = {};
 
 Promise.all([
-  fetch('esgt_events.md').then(res => res.text()),
-  fetch('univ_events.md').then(res => res.text())
-])
-.then(([esgtText, univText]) => {
-  events['ESGT'] = eval('[' + esgtText + ']');
-  events['UNIV'] = eval('[' + univText + ']');
-  createCalendarGrid(); // relancer le rendu après chargement
-});
-
-
-if (typeof events === 'undefined') var events = {};
-
-Promise.all([
   fetch('esgt_events.json').then(res => res.json()),
   fetch('univ_events.json').then(res => res.json())
 ])
