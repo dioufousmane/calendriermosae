5d3a545a67751d18baacd971e5eb60eccd7005e8
Bassmbacke <dioufstory@gmail.com>
2025-07-22 15:03:21 +0200

    all

--- script2.js
+++ script2.js
@@ -104,16 +104,8 @@ function renderEvents() {
         });
     });
 }
-function forceNoCacheReload() {
-    showTemporaryMessage("Chargement en cours… sans cache 🧹");
 
-    const url = new URL(window.location.href);
-    url.searchParams.set('_', Date.now());
 
-    setTimeout(() => {
-      window.location.href = url.toString();
-    }, 1500);
-  }
 
   function showTemporaryMessage(text) {
     const message = document.createElement('div');
